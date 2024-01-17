33c928c0e6bf26923f4a8e466721dd77a3662f5a
fraco92 <fraco92@gmail.com>
2024-01-17 18:54:09 +0100

    Added Hide Button and refactor

--- script.js
+++ script.js
@@ -2,28 +2,26 @@ let dataBooks;
 const bookList = document.getElementById("bookList");
 const searchInput = document.getElementById("searchInput");
 
-fetch("https://striveschool-api.herokuapp.com/books")
-  .then((response) => response.json())
-  .then((data) => {
-    dataBooks = data;
-    showBooks();
-  })
-  .catch((error) => console.error(error));
-
 const getBooks = () => {
   const searchFilter = searchInput.value.toLowerCase();
   if (dataBooks) {
-    return dataBooks.filter((book) =>
-      book.title.toLowerCase().includes(searchFilter)
+    return dataBooks.filter(
+      (book) => !book.hidden && book.title.toLowerCase().includes(searchFilter)
     );
   } else {
     return [];
   }
 };
 
-const addToCart = (bookTitle) => {
-  const book = dataBooks.findIndex((book) => book.title == bookTitle);
+const toggleCart = (bookTitle) => {
+  const book = dataBooks.findIndex((book) => book.title === bookTitle);
   dataBooks[book].inCart = !dataBooks[book].inCart;
+  showBooks();
+};
+
+const hideBook = (bookTitle) => {
+  const book = dataBooks.findIndex((book) => book.title === bookTitle);
+  dataBooks[book].hidden = true;
   showBooks();
 };
 
@@ -33,11 +31,13 @@ const showBooks = () => {
 
   books.forEach((book) => {
     let inCartStyle = "";
+    let cartAction = "Add to";
     const card = document.createElement("div");
     card.classList.add("col-md-4");
 
     if (book.inCart) {
       inCartStyle = "border-warning";
+      cartAction = "Remove from";
     }
 
     card.innerHTML = `
@@ -47,7 +47,8 @@ const showBooks = () => {
             <h5 class="card-title">${book.title}</h5>
             <p class="card-text mb-1">Price: ${book.price} €</p>
             <p class="card-text mb-1">Category: ${book.category}</p>
-            <a class="btn btn-primary" onclick="addToCart('${book.title}')">Cart</a>
+            <a class="btn btn-primary" onclick="hideBook('${book.title}')">Hide</a>
+            <a class="btn btn-primary" onclick="toggleCart('${book.title}')">${cartAction} Cart</a>
           </div>
         </div>
       `;
@@ -55,6 +56,14 @@ const showBooks = () => {
     bookList.appendChild(card);
   });
 };
+
+fetch("https://striveschool-api.herokuapp.com/books")
+  .then((response) => response.json())
+  .then((data) => {
+    dataBooks = data;
+    showBooks();
+  })
+  .catch((error) => console.error(error));
 
 searchInput.addEventListener("input", () => {
   showBooks();
