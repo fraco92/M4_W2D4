7a39fbb5e0c710142c65f50b6723884e74a7c86c
fraco92 <fraco92@gmail.com>
2024-01-17 19:32:26 +0100

    complete displayBook function: innerHTML added

--- details.js
+++ details.js
@@ -2,14 +2,41 @@ const params = new URLSearchParams(location.search);
 const id = params.get("id");
 const bookContainer = document.getElementById("bookDetails__container");
 
-const displayBook = (book) => {};
+/**
+ *
+ * @param {{asin: string, title: string, img: string, price: number, title: string, category: string}} book
+ */
+const displayBook = ({ asin, price, img, title, category }) => {
+  bookContainer.innerHTML = `<div class="mb-3">
+  <div class="row g-0">
+    <div class="col-md-6">
+      <img
+        src="${img}"
+        class="img-fluid rounded-start"
+        alt="${title}"
+      />
+    </div>
+    <div class="col-md-6">
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <h6 class="card-text">${category}</h6>
+        <p class="card-text">Asin: 
+        ${asin}
+                </p>
+        <p class="card-text">Prezzo: €
+${price}
+        </p>
+      </div>
+    </div>
+  </div>
+  </div>`;
+};
 
 if (id) {
   fetch("https://striveschool-api.herokuapp.com/books/" + id)
     .then((response) => response.json())
     .then((data) => {
       displayBook(data);
-      console.log(data);
     })
     .catch((error) => console.error(error));
 }
