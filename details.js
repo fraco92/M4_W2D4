98afaa805ddaea778270ff6eb8b7a47ab00608c4
fraco92 <fraco92@gmail.com>
2024-01-18 18:25:08 +0100

    card update

--- details.js
+++ details.js
@@ -7,29 +7,21 @@ const bookContainer = document.getElementById("bookDetails__container");
  * @param {{asin: string, title: string, img: string, price: number, title: string, category: string}} book
  */
 const displayBook = ({ asin, price, img, title, category }) => {
-  bookContainer.innerHTML = `<div class="mb-3">
+  bookContainer.innerHTML = `<div class="card mb-3" style="max-width: 540px">
   <div class="row g-0">
-    <div class="col-md-6">
-      <img
-        src="${img}"
-        class="img-fluid rounded-start"
-        alt="${title}"
-      />
+    <div class="col-md-4">
+      <img src="${img}" class="img-fluid rounded-start" alt="${title}" />
     </div>
-    <div class="col-md-6">
+    <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">${title}</h5>
-        <h6 class="card-text">${category}</h6>
-        <p class="card-text">Asin: 
-        ${asin}
-                </p>
-        <p class="card-text">Prezzo: €
-${price}
-        </p>
+      <h5 class="card-title">${title}</h5>
+      <h6 class="card-text">${category}</h6>
+      <p class="card-text">Asin: ${asin}</p>
+      <p class="card-text">Prezzo: €${price}</p>
       </div>
     </div>
   </div>
-  </div>`;
+</div>`;
 };
 
 if (id) {
